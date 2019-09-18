c4915ef57bb1d2d76fe77c01ad2a0fc41671b5ec
League Student <league@iMac-37.attlocal.net>
2019-09-17 19:56:38 -0700

    Create GG.java
    eeewewrR

---
+++ src/_14_rollercoaster/GG.java
@@ -0,0 +1,52 @@
+package _14_rollercoaster;
+
+import javax.swing.JOptionPane;
+
+public class GG {public static void main(String[] args) {
+	String a = JOptionPane.showInputDialog("how tall are you in inches");
+	int h =Integer.parseInt(a);
+if(h < 48) {
+	JOptionPane.showMessageDialog("grow taller");
+	
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+
+
+}}

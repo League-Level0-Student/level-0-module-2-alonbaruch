bba27d246b7443e9d807f7d8cc3aea97bf1c8c46
League Student <league@iMac-17.attlocal.net>
2019-09-24 18:57:46 -0700

    nm h
    hkyjl,

--- src/_14_rollercoaster/GG.java
+++ src/_14_rollercoaster/GG.java
@@ -6,11 +6,20 @@ public class GG {public static void main(String[] args) {
 	String a = JOptionPane.showInputDialog("how tall are you in inches");
 	int h =Integer.parseInt(a);
 if(h < 48) {
-	JOptionPane.showMessageDialog("grow taller");
+	JOptionPane.showMessageDialog(null,"grow taller");
+	
 	
 
 }
 	
+
+int g =Integer.parseInt(a);
+if(g > 48) {
+JOptionPane.showMessageDialog(null,"Your to big");
+
+
+
+}
 	
 	
 	
---
+++ src/_15_voting_booth/dfgh.java
@@ -0,0 +1,25 @@
+package _15_voting_booth;
+
+import javax.swing.JOptionPane;
+
+public class dfgh {public static void main(String[] args) {
+	String a = JOptionPane.showInputDialog("how old are you");
+	int h =Integer.parseInt(a);
+if(h > 17) {
+	JOptionPane.showInputDialog(null,"who do you think should be the next president");
+	
+	
+
+}
+	
+
+
+if(h < 17) {
+JOptionPane.showMessageDialog(null,"YOUR TO SMALL PLAY FORTNITE");
+
+
+
+}}}
+	
+
+
